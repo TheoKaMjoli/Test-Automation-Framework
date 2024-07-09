d47d372a3d2bd0d9e001af88fc425a50dc161d5e
Theo <theoMzoxolo.Nukwayo@OMfinance.co.za>
2024-07-09 14:01:45 +0200

    Reran webdrivermanager

--- src/test/java/workSpace/TestingWebdriver.java
+++ src/test/java/workSpace/TestingWebdriver.java
@@ -1,13 +1,18 @@
 package workSpace;
 
 
+import static java.lang.invoke.MethodHandles.lookup;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.time.LocalDate;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import omf.summit.Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.UnexpectedAlertBehaviour;
@@ -26,12 +31,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestingWebdriver {
 	String sutUrl = "https://bonigarcia.dev/selenium-webdriver-java/web-form.html";
-	WebDriver driver;
-	
+	static final Logger log = getLogger(lookup().lookupClass());
+	String title;
+	private WebDriver driver;
+
 	@BeforeMethod
 	void classSetup() {
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\X483094\\.cache\\selenium\\chromedriver\\win64\\126.0.6478.126.chromedriver.exe");
+		WebDriverManager.edgedriver().create();
 
-	 WebDriverManager.edgedriver().create();
 	 
 	EdgeOptions browserOptions = new EdgeOptions();
 	DesiredCapabilities cap = new DesiredCapabilities();
@@ -44,91 +52,90 @@ public class TestingWebdriver {
 		driver.get(sutUrl);
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
+		title = driver.getTitle();
 		
-		assertThat(driver.getTitle()).isEqualTo("Hands-On Selenium WebDriver with Java");
+		assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java");
 		
 	}
 	
 	@Test
 	void runTest(){
-		
+
 		//getting the current date from the system clock
 		LocalDate today = LocalDate.now();
 		int currentYear = today.getYear();
 		int currentDay = today.getDayOfMonth();
-		
-		
-		//verify the title of the page 
-		if(assertThat(driver.getTitle()).isEqualTo("Hands-On Selenium WebDriver with Java") != null) {
-			
-			System.out.println("The title of the page is: " + driver.getTitle());		
-					
+
+
+		//verify the title of the page
+		if(assertThat(title).isEqualTo("Hands-On Selenium WebDriver with Java") != null) {
+
+			log.debug("The title of {} is {}",sutUrl  ,title);
+
 		}
-		
+
 		//click to open the calendar
 		WebElement datePicker = driver.findElement(By.xpath("//input[@name='my-date']"));
 		datePicker.click();
-		
-		//click 
+
+		//click
 		WebElement monthElement = driver.findElement(By.xpath(String.format("//th[contains(text(),'%d')]", currentYear)));
 		monthElement.click();
-		
+
 		//Click on the left arrow of the calendar
 		WebElement arrowLeft = driver.findElement(RelativeLocator.with(By.tagName("th")).toRightOf(monthElement));
 		arrowLeft.click();
-		
+
 		/*We will now play around with the sliders*/
-		
+
 		WebElement rangeSlider = driver.findElement(By.name("my-range"));
-		
+
 		String initValue = rangeSlider.getAttribute("value");
-		System.out.println("Initial value of the slider is " + initValue );
-		
+		log.debug("Initial value of the slider is {}", initValue );
+
 		for(int i = 0; i < 5; i++) {
-			
+
 			rangeSlider.sendKeys(Keys.ARROW_RIGHT);
-			
+
 		}
-		
+
 		String endValue = rangeSlider.getAttribute("value");
 		assertThat(initValue).isNotEqualTo(endValue);
-		
+
 		//click on the second checkbox
-		
+
 		WebElement checkbox2 = driver.findElement(By.id("my-check-2"));
-		
+
 		checkbox2.click();
 		assertThat(checkbox2.isSelected()).isTrue();
-		
-		//Upload file 
-		
+
+		//Upload file
+
 		WebElement inputFile = driver.findElement(By.name("my-file"));
 		try {
-			
-			
+
+
 			Path tempFile = Files.createTempFile("tempfiles", "tmp");
 			String  fileName = tempFile.toAbsolutePath().toString();
-			
+
 			inputFile.sendKeys(fileName);
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		driver.findElement(By.tagName("form")).submit();
-		
+
 		assertThat(driver.getCurrentUrl()).isNotEqualTo(sutUrl);
-		
-		
+
+
 	}
 	
 	@AfterMethod
 	void closeBrowsers() {
-		
-		//driver.close();
-//		driver.quit();
+		driver.close();
+		driver.quit();
 	}
-	
 
 }
